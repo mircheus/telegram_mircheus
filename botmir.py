d4f55ceb32709301ea28a9d416a06a09c97f4dc7
mircheus <mircheus@mail.ru>
2016-11-12 17:44:10 +0300

    Добавили пару первых файлов Добавили файл БД

--- botmir.py
+++ botmir.py
@@ -3,6 +3,20 @@ import config
 
 # Настраиваем соединение с нашим ботом
 
-class Bot:
-    a = 10
-    b = 12
+bot = telebot.TeleBot(config.token)
+
+# Бот отвечает на команды
+
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+    bot.reply_to(message, "Howdy, how are you doing?")
+
+# Бот отвечает на все сообщения по умолчанию
+
+@bot.message_handler(func=lambda m: True)
+def echo_all(message):
+    bot.reply_to(message, message.text)
+
+# запускаем бота
+
+bot.polling()
